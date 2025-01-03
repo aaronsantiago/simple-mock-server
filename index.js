c34c740774653ec91d9b12c397d3030d178b1c63
Aaron Santiago <me@aaron.work>
2025-01-02 21:34:24 -0500

    working version with delivering files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 import fs from 'fs';
+import fsp from 'fs/promises';
 import cors from "cors";
 import process from "process";
 import express from "express";
@@ -15,161 +16,9 @@ if (process.argv.length > 2) {
 let config = JSON.parse(fs.readFileSync(configPath, "utf8"));
 console.log(config);
 
-let serverConnectionCount = [];
-let serverConnectionQueue = [];
-
-for (let _ of config.destinationServers) {
-  serverConnectionCount.push(0);
-}
-
 app.use(express.json({limit: "500mb"})); // for parsing application/json
 app.use(express.urlencoded({limit: "500mb"}));
 app.use(cors());
-
-let parseDbData = [];
-
-if (config.useParseDb) {
-  setInterval(async () => {
-    const url = config.parseDbUrl;
-    const headers = {
-      Accept: "application/json",
-      "X-Parse-Application-Id": config.parseDbAppId,
-      "X-Parse-REST-API-Key": config.parseDbRestApiKey,
-      "Content-Type": " application/json",
-    };
-    // let queryParams = "?where=" + JSON.stringify({tulpa_id: tulpaId});
-    let queryParams = "";
-
-    try {
-      let response = await fetch(url + queryParams, {
-        method: "GET", // or 'PUT'
-        headers: headers,
-      });
-      let data = await response.json();
-      parseDbData = data.results;
-      // console.log(data);
-    } catch (error) {
-      if (error.name === "AbortError") {
-        console.log("Fetch aborted");
-        return;
-      }
-      console.error("Error:", error);
-    }
-  }, 1000);
-}
-
-async function executeRequest(req, res, serverIndex, method) {
-  serverConnectionCount[serverIndex] += 1;
-
-  try {
-    console.log("executing request", config.destinationServers[serverIndex] + req.url);
-    let modifiedHeaders = {...req.headers};
-    
-    delete modifiedHeaders["content-length"];
-    
-    let modifiedBody = {};
-
-    for (let key of Object.keys(req.body)) {
-      if (!key.startsWith("snq")) {
-        modifiedBody[key] = req.body[key];
-      }
-    }
-    let params = 
-    {
-      method: method,
-      headers: modifiedHeaders,
-    };
-
-    if (method != "GET") {
-      params.body = JSON.stringify(modifiedBody)
-    }
-
-    let response = await fetch(
-      "http" + (config.useHttps ? "s" : "") +  "://" + config.destinationServers[serverIndex] + req.url,
-      params
-    );
-    let blob = await response.blob();
-    res.send(Buffer.from(await blob.arrayBuffer()));
-  }
-  catch (e) {
-    console.log("error with request");
-    console.log(e);
-    res.end("error");
-  }
-  serverConnectionCount[serverIndex] -= 1;
-}
-
-function getAvailableServerIndex() {
-  let minimum = config.maximumRequestsPerServer;
-  let minimumIndex = -1;
-  let minimumIndices = [];
-  for (let i = 0; i < serverConnectionCount.length; i++) {
-    if (config.randomizeDestinationServer) {
-      if (serverConnectionCount[i] < minimum) {
-        minimum = serverConnectionCount[i];
-        minimumIndices = [];
-      }
-      if (serverConnectionCount[i] == minimum) {
-        minimumIndices.push(i);
-      }
-    }
-    else {
-      if (serverConnectionCount[i] < minimum) {
-        minimum = serverConnectionCount[i];
-        minimumIndex = i;
-      }
-    }
-  }
-
-  if (config.randomizeDestinationServer && minimumIndices.length > 0) {
-    minimumIndex = minimumIndices[Math.floor(Math.random() * minimumIndices.length)];
-  }
-
-  return minimumIndex;
-}
-
-let waitingForDebounce = false;
-function addToQueue(callback) {
-  serverConnectionQueue.push(callback);
-  if (!waitingForDebounce) {
-    waitingForDebounce = true;
-    (async () => {
-      await sleep(config.requestDebounceTime);
-      waitingForDebounce = false;
-      processQueue();
-    })();
-  }
-}
-
-async function processQueue() {
-  while(serverConnectionQueue.length > 0) {
-    let serverIndex = getAvailableServerIndex();
-    if (serverIndex < 0) return;
-    serverConnectionCount[serverIndex] += 1;
-    let connectionIndex = 0;
-    if (config.usePriorityField) {
-      let priority = Infinity;
-      for (let i = 0; i < serverConnectionQueue.length; i++) {
-        let connectionPriority = serverConnectionQueue[i].priority;
-        if (config.useParseDb) {
-          let dbValue = parseDbData.find((x) => x[config.parseDbDbField] == serverConnectionQueue[i].dbField);
-          dbValue = dbValue[config.parseDbValueField];
-          connectionPriority -= dbValue;
-          console.log("calculating priority", connectionPriority, serverConnectionQueue[i].dbField, serverConnectionQueue[i].priority, dbValue)
-        }
-        if (connectionPriority < priority) {
-          connectionIndex = i;
-          priority = connectionPriority;
-        }
-      }
-    }
-    let callback = serverConnectionQueue.splice(connectionIndex, 1)[0].callback;
-    await callback(serverIndex);
-    serverConnectionCount[serverIndex] -= 1;
-  }
-}
-
-let bundledRequests = {};
 
 app.post("*", (req, res) => {
   handleRequest(req, res, "POST");
@@ -187,58 +36,59 @@ app.options("*", (req, res) => {
   handleRequest(req, res, "OPTIONS");
 });
 
-function handleRequest(req, res, method) {
-  let serverConnection = {callback: async (serverIndex) => {
-    await executeRequest(req, res, serverIndex, method);
-  }};
-  if (config.usePriorityField) {
-    serverConnection.priority = req.body[config.priorityField];
-  }
-  if (config.useParseDb) {
-    serverConnection.dbField = req.body[config.parseDbRequestField];
-  }
-  if (req.body["snqBundleId"]) {
-    let bid = req.body["snqBundleId"];
-    if (!bundledRequests[bid]) {
-      bundledRequests[bid] = [];
-    }
-    serverConnection.bundleOrder = req.body["snqBundleOrder"];
-    if (serverConnection.bundleOrder != null) {
-      let insertionFound = false;
-      for (let i = 0; i < bundledRequests[bid].length; i++) {
-        console.log("checking bundle order", bundledRequests[bid][i].bundleOrder, serverConnection.bundleOrder);
-        if (bundledRequests[bid][i].bundleOrder > serverConnection.bundleOrder) {
-          bundledRequests[bid].splice(i, 0, serverConnection);
-          insertionFound = true;
-          break;
-        }
+async function convertObject(obj, index){
+  console.log(obj, index);
+  let newObj = {};
+  for(let key of Object.keys(obj))
+  {
+    if (typeof obj[key] === "object") {
+      newObj[key] = await convertObject(obj[key], index);
+    } else if (typeof obj[key] === "string") {
+      let value = obj[key];
+      while (value.includes(config.responseReplacementIndicator + config.responseReplacementOpenBracket)) {
+        let start = value.indexOf(config.responseReplacementIndicator + config.responseReplacementOpenBracket);
+        let end = value.indexOf(config.responseReplacementCloseBracket, start);
+        let variable = value.substring(start + config.responseReplacementIndicator.length + config.responseReplacementOpenBracket.length, end);
+
+        let index = parseInt(variable);
+        console.log(variable, config, config.files, config.files[0], index, config.files[index]);
+        let replacement = await fsp.readFile(config.files[index], {encoding: 'base64'});
+        value = value.substring(0, start) + replacement + value.substring(end + config.responseReplacementCloseBracket.length);
       }
-      if (!insertionFound) {
-        bundledRequests[bid].push(serverConnection);
-      }
+      newObj[key] = value;
     }
     else {
-      bundledRequests[bid].push(serverConnection);
-    }
-    console.log("adding to bundle", req.body["snqBundleId"], bundledRequests[req.body["snqBundleId"]].length);
-    if (bundledRequests[bid].length == req.body["snqBundleSize"]) {
-      console.log("adding bundle to queue", req.body["snqBundleId"]);
-      addToQueue({
-        callback: async (serverIndex) => {
-          for (let i = 0; i < bundledRequests[bid].length; i++) {
-            await bundledRequests[bid][i].callback(serverIndex);
-          }
-          delete bundledRequests[bid];
-        },
-        priority: serverConnection.priority,
-        dbField: serverConnection.dbField
-      });
+      newObj[key] = obj[key];
     }
   }
-  else {
-    console.log("adding to queue:", serverConnection);
-    addToQueue(serverConnection)
+  return newObj;
+}
+
+let currentFileIndex = 0;
+async function handleRequest(req, res, method) {
+  let fileIndex = currentFileIndex++;
+  if (config.randomizeOrder) {
+    fileIndex = Math.floor(Math.random() * config.files.length);
   }
+  let waitTime = config.baseTime + Math.random() * config.addRandomTime;
+  await sleep(waitTime);
+
+  try {
+      console.log("executing request", req.url);
+      if (config.asJson) {
+        let response = structuredClone(config.responseFormat);
+        response = await convertObject(response, fileIndex);
+        res.json(response);
+      }
+      else {
+        res.sendFile(config.files[fileIndex]);
+      }
+    }
+    catch (e) {
+      console.log("error with request");
+      console.log(e);
+      res.end("error");
+    }
 };
 
 let port = config.port;
